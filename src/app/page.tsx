c86a86bc3f064da846d428b19a913d92d5088940
Raisa <raisa.rmro@gmail.com>
2025-05-05 18:29:37 -0300

    lenis added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import ContactSection from "@/components/ui/ContactSection";
 import Finalizadora from "@/components/ui/Finalizadora";
 import Footer from "@/components/ui/Footer";
@@ -6,9 +8,22 @@ import ServicosSection from "@/components/ui/ServicosSection";
 import TestimonialSection from "@/components/ui/TestemonialSection";
 import ValorSection from "@/components/ui/ValorSection";
 import ValorSection2 from "@/components/ui/ValorSection2";
+import Lenis from "lenis";
+import { useEffect } from "react";
 
 
 export default function Home() {
+
+  useEffect(() => {
+    const lenis = new Lenis();
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
+
+
   return (
     <div className="">
       <main className="">
